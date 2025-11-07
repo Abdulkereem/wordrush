2f733af1441dc7c59220ece61f593e7a7a145d8f
abdulkereemokereem <abdulkereemokereem@gmail.com>
2025-11-07 12:49:36 +0000

    go

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [
-    sveltekit(),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(),
-      ]
-    }
-  }
+  plugins: [sveltekit()]
 });
